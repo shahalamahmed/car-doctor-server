6f4b5cd79fb94cf3c9d4282f39e5199cee6dfaf1
shahalamahmed <fcshahamol@gmail.com>
2024-05-04 23:35:53 +0600

    added jwt fuction

---
+++ _jwt_steps_.js
@@ -0,0 +1,34 @@
+/****
+ * 
+ * install jsonwebtoken
+ * jwt.sign(payload , secret , {expiresIn: })
+ * token client
+ */
+
+/**
+ * how to store token in the client side 
+ * 1.memory  --> ok type 
+ * 2.local storage --> ok type 
+ * 3. cookies: http only 
+ */
+
+/**
+ * 1. set cookies with http only . for development secure: false;
+ * 2. cors
+ *  app.use(cors({
+    origin: [
+        'http://localhost:5173',
+       
+    ],
+    credentials: true
+}));
+ * 3. client side axios setting 
+ * in axios set withCredentials : true
+ */
+
+
+/***
+ * 
+ * 1. to send cookies from te client make sure you added with Credentials : true for the call using axios
+ * 2. use cookie parser as middleware
+ */
--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware 
-app.use(cors());
+app.use(cors({
+    origin: [
+        'http://localhost:5173',
+       
+    ],
+    credentials: true
+}));
+
 app.use(express.json());
+
+app.use(cookieParser());
 
 
 
@@ -23,6 +34,8 @@ const client = new MongoClient(uri, {
     }
 });
 
+// middleware
+
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
@@ -30,7 +43,22 @@ async function run() {
 
         const serviceCollection = client.db('carsDoctor').collection('services');
         const bookingCollection = client.db('carsDoctor').collection('bookings')
+        // auth related api
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            console.log(user);
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            res
+                .cookie('token', token, {
+                    httpOnly: true,
+                    secure: false,
+                  
+                })
+                .send({ success: true })
+         
+        })
 
+        // services related api
         app.get('/services', async (req, res) => {
             const cursor = serviceCollection.find();
             const result = await cursor.toArray();
@@ -54,6 +82,7 @@ async function run() {
         //booking 
         app.get('/bookings', async (req, res) => {
             console.log(req.query.email);
+            console.log('tok tok token' , req.cookies.token);
             let query = {};
             if (req.query?.email) {
                 query = { email: req.query.email }
