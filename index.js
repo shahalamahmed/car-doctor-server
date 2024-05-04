5526fff77dceff37b192ede2243ac525fc743ec6
shahalamahmed <fcshahamol@gmail.com>
2024-05-04 12:16:51 +0600

    added delete and update and post section

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ async function run() {
 
             const options = {
 
-                projection: { title: 1, price: 1, service_id: 1 , img: 1},
+                projection: { title: 1, price: 1, service_id: 1, img: 1 },
             };
             const result = await serviceCollection.findOne(query, options);
             res.send(result)
@@ -55,8 +55,8 @@ async function run() {
         app.get('/bookings', async (req, res) => {
             console.log(req.query.email);
             let query = {};
-            if(req.query?.email){
-                query ={email: req.query.email}
+            if (req.query?.email) {
+                query = { email: req.query.email }
             }
             const result = await bookingCollection.find(query).toArray();
             res.send(result);
@@ -64,8 +64,29 @@ async function run() {
         app.post('/bookings', async (req, res) => {
             const booking = req.body;
             console.log(booking);
-            const result = await bookingCollection.insertOne(booking)
+            const result = await bookingCollection.insertOne(booking);
             res.send(result)
+
+        })
+        app.patch('/bookings/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const updatedBooking = req.body;
+            console.log(updatedBooking);
+            const updateDoc = {
+                $set: {
+                    status: updatedBooking.status
+                },
+            };
+            const result = await bookingCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        })
+
+        app.delete('/bookings/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await bookingCollection.deleteOne(query);
+            res.send(result);
 
         })
 
